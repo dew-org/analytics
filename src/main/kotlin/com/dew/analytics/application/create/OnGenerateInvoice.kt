2e2a32684a1423565c0d24f219f28c729d468a13
Carlos Pavajeau <cantte098@gmail.com>
2022-10-25 16:24:11 -0500

    refactor: rewrite event logic with pub/sub

--- src/main/kotlin/com/dew/analytics/application/create/OnGenerateInvoice.kt
+++ src/main/kotlin/com/dew/analytics/application/create/OnGenerateInvoice.kt
@@ -1,18 +1,19 @@
 package com.dew.analytics.application.create
 
 import com.dew.common.domain.bus.Mediator
-import io.micronaut.configuration.kafka.annotation.KafkaListener
-import io.micronaut.configuration.kafka.annotation.Topic
 import io.micronaut.context.annotation.Requires
 import io.micronaut.context.env.Environment
-import reactor.core.publisher.Flux
+import io.micronaut.gcp.pubsub.annotation.PubSubListener
+import io.micronaut.gcp.pubsub.annotation.Subscription
 import reactor.core.publisher.Mono
 
 @Requires(notEnv = [Environment.TEST])
-@KafkaListener
+@PubSubListener
 class OnGenerateInvoice(private val mediator: Mediator) {
 
-    @Topic("generate-invoice")
-    fun onGenerateInvoice(invoice: GeneratedInvoice): Flux<Boolean> =
-        mediator.send(CreateAnalyticsCommand(invoice.products, invoice.total, invoice.userId))
+    @Subscription("generate-invoice")
+    fun onGenerateInvoice(invoice: GeneratedInvoice) {
+        Mono.from(mediator.send(CreateAnalyticsCommand(invoice.products, invoice.total, invoice.userId)))
+            .block()
+    }
 }
